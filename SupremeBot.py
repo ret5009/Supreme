fcba4baf98b0c38eb6c4134fe4998de4a44f082e
ret5009 <48139115+ret5009@users.noreply.github.com>
2019-03-05 01:43:51 +0900

    Update SupremeBot.py

--- SupremeBot.py
+++ SupremeBot.py
@@ -23,9 +23,6 @@ async def on_message(message):
     if message.content.startswith('!도움말'):
        await client.send_message(message.channel, " ```팀나누기 = !팀나누기 멤버1 멤버2/팀번호1 팀번호2\n투표 = !투표 투표내용/투표목록1/투표목록2\n주사위 = !주사위 2x6 (6면체 주사위를 2번 굴려라)\n골라 = !골라 짜장면 짬뽕\n음식추천 = !뭐먹지\n관리자호출 = !관리자\n도움말에 없는 숨겨진 명령어를 찾으면 소정의상품증정(베라싱글기프트콘)힌트는 관리자에게 DM ``` ")
 
-    if message.content.startswith('!관리자들'):
-        await client.send_message(message.channel, "일좀해라")
-
     if message.content.startswith('!인재는잘생김'):
         await client.send_message(message.channel, "인정")
 
